93cde140e535f215d7c1497d634dcc73befa1a90
mintchoco1 <ldj2356@gmail.com>
2025-03-07 22:25:15 +0900

    desktop commit

--- datastructure/ch04/Prob04-2/DLinkedRead.c
+++ datastructure/ch04/Prob04-2/DLinkedRead.c
@@ -20,7 +20,7 @@ int main(void)
 	head = (Node*)malloc(sizeof(Node));    // 추가 된 문장. 더미 노드 추가. 더미 노드를 헤드가 가리킴
 	tail = head;	
 
-	/****  ****/
+	/*데이터를 입력받는 과정*/
 	while(1)
 	{
 		printf("자연수 입력: ");
@@ -49,14 +49,14 @@ int main(void)
 	printf("입력 받은 데이터의 전체 출력! \n");
 	if(head == NULL) 
 	{
-		printf("����� �ڿ����� �������� �ʽ��ϴ�. \n");
+		printf("저장된 자연수가 존재하지 않습니다. \n");
 	}
 	else 
 	{
 		cur = head; 
-	//	printf("%d  ", cur->data);   // ù ��° ������ ���
+	//	printf("%d  ", cur->data);   // 첫 번째 데이터 출력
 		
-		while(cur->next != NULL)    // �� ��° ������ ������ ���
+		while(cur->next != NULL)    // 두 번째 이후의 데이터 출력 위한 반복문
 		{
 			cur = cur->next;
 			printf("%d  ", cur->data);
@@ -64,17 +64,17 @@ int main(void)
 	}
 	printf("\n\n");
 
-	/**** �޸��� �������� ****/
+	/**** 메모리 해제 과정****/
 	if(head == NULL) 
 	{
-		return 0;    // ������ ��尡 �������� �ʴ´�.
+		return 0;    // 해제할 노드가 존재하지 않는다
 	}
 	else 
 	{
 		Node * delNode = head;
 		Node * delNextNode = head->next;
 
-	//	printf("%d��(��) �����մϴ�. \n", head->data);
+	//	printf("%d를 삭제합니다. \n", head->data);
 	//	free(delNode);    // 첫 번째 노드의 삭제
 		
 		while(delNextNode != NULL)    // 두 번째 이후의 노드 삭제 위한 반복문
@@ -82,7 +82,7 @@ int main(void)
 			delNode = delNextNode;
 			delNextNode = delNextNode->next;
 
-			printf("%d��(��) �����մϴ�. \n", delNode->data);
+			printf("%d를 삭제합니다. \n", delNode->data);
 			free(delNode);    // 두 번째 이후의 노드 삭제
 		}
 	}
